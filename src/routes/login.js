12f3c916777ad80e7dc1da5809155144001fc6b8
Kyle Nance <nancekyle15@gmail.com>
2021-06-19 12:20:14 -0400

    modidifed env

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,4 +3,12 @@ import Form from "react-bootstrap/Form";
 import Button from "react-boostrap/Button";
 import "Login.css"; //needs to be changed to our actual css file
 
-expo
+function login() {
+    retun (
+        <div className="login" >
+            <h1>Login Page</h1>
+        </div>
+    );
+}
+
+export  default login;
