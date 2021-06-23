581d5c790a596b98b3ce5110d14ea9bf544c0b03
Doug <hathemagicpenguin@gmail.com>
2021-06-22 20:58:52 -0400

    Merge remote-tracking branch 'origin/backendoug' into backendoug

--- appStart/client/src/index.js
+++ appStart/client/src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
