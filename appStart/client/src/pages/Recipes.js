585dc9f0e316439811684aee67ef9c9e8bfaddfd
Doug <hathemagicpenguin@gmail.com>
2021-06-26 10:57:22 -0400

    Server Fix

--- appStart/client/src/pages/Recipes.js
+++ appStart/client/src/pages/Recipes.js
@@ -1,31 +1,21 @@
 import React from "react";
 
-
 /**
- *
+ * @param props
  */
- // eslint-disable-next-line space-before-blocks
- function Recipes(props){
-   return(
-     <div>
-       <h2> The Recipes we are serving today</h2>
-       <ul>
-   {props.recipe.map(item=>(
-     // eslint-disable-next-line react/jsx-key
-     <li>{item.title}</li>
-   ))}
- </ul>
+// eslint-disable-next-line space-before-blocks
+function Recipes(props) {
+  return (
+    <div>
+      <h2> The Recipes we are serving today</h2>
+      <ul>
+        {props.recipe.map((item) => (
+          // eslint-disable-next-line react/jsx-key
+          <li>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
-
-     </div>
- 
-   )
- }
-
-
- export default Recipes;
-
-
-
- 
-  
+export default Recipes;
