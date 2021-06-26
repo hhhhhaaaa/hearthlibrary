677c3406d5256ecb32e11a06054f229133b6f2bd
Doug <hathemagicpenguin@gmail.com>
2021-06-26 11:31:58 -0400

    More Build Fixes

--- appStart/client/src/pages/Recipes.js
+++ appStart/client/src/pages/Recipes.js
@@ -2,24 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 /**
-<<<<<<< Updated upstream
- *
- */
-// eslint-disable-next-line space-before-blocks
-function Recipes(props) {
-  return (
-    <div>
-      <h2> The Recipes we are serving today</h2>
-      <ul>
-        {props.recipe.map((item) => (
-          // eslint-disable-next-line react/jsx-key
-          <li>{item.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-=======
  * @param props
  * @param props.recipe
  */
@@ -43,4 +25,3 @@ Recipes.propTypes = {
 };
 
 export default Recipes;
->>>>>>> Stashed changes
