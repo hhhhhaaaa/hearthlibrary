2593eda49532110eb74a3f549aa9f10726b39522
Doug <hathemagicpenguin@gmail.com>
2021-06-26 12:56:02 -0400

    File Loading Fix

--- appStart/client/config/webpack.config.dev.js
+++ appStart/client/config/webpack.config.dev.js
@@ -86,7 +86,8 @@ module.exports = {
       // eslint-disable-next-line function-paren-newline
     ].concat(
       // It is guaranteed to exist because we tweak it in `env.js`
-      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)),
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+    ),
 
     /* These are the reasonable defaults supported by the Node ecosystem.
        We also include JSX as a common component filename extension to support
@@ -94,14 +95,7 @@ module.exports = {
        https://github.com/facebookincubator/create-react-app/issues/290
        `web` extension prefixes have been added for better support
        for React Native Web. */
-    extensions: [
-".web.js",
-".mjs",
-".js",
-".json",
-".web.jsx",
-".jsx"
-],
+    extensions: [".web.js", ".mjs", ".js", ".json", ".web.jsx", ".jsx"],
     alias: {
 
       /* Support React Native Web
@@ -147,16 +141,11 @@ module.exports = {
              smaller than specified limit in bytes as data URLs to avoid requests.
              A missing `test` is equivalent to a match. */
           {
-            test: [
-/\.bmp$/u,
-/\.gif$/u,
-/\.jpe?g$/u,
-/\.png$/u
-],
+            test: [/\.bmp$/u, /\.gif$/u, /\.jpe?g$/u, /\.png$/u, /\.svg$/u],
             loader: "url-loader",
             options: {
               limit: 10000,
-              name: "static/media/[name].[hash:8].[ext]"
+              name: "static/media/[name].[fullhash:8].[ext]"
             },
             exclude: ["/src/seed/"]
           },
@@ -216,29 +205,13 @@ module.exports = {
               }
             ],
             exclude: ["/src/seed/"]
-          },
+          }
 
           /* "file" loader makes sure those assets get served by WebpackDevServer.
              When you `import` an asset, you get its (virtual) filename.
              In production, they would get copied to the `build` folder.
              This loader doesn't use a "test" so it will catch all modules
              that fall through the other loaders. */
-          {
-
-            /* Exclude `js` files to keep "css" loader working as it injects
-               its runtime that would otherwise processed through "file" loader.
-               Also exclude `html` and `json` extensions so they get processed
-               by webpacks internal loaders. */
-            exclude: [
-jsTest,
-/\.html$/u,
-/\.json$/u
-],
-            options: {
-              name: "static/media/[name].[hash:8].[ext]"
-            },
-            loader: "file-loader"
-          }
         ]
       }
 
@@ -283,13 +256,7 @@ jsTest,
        You can remove this if you don't use Moment.js: */
     new webpack.IgnorePlugin(/^\.\/locale$/u, /moment$/u),
     new ESLintPlugin({
-      extensions: [
-"js",
-"mjs",
-"jsx",
-"ts",
-"tsx"
-],
+      extensions: ["js", "mjs", "jsx", "ts", "tsx"],
       formatter: require.resolve("react-dev-utils/eslintFormatter"),
       eslintPath: require.resolve("eslint"),
       context: paths.appSrc,
