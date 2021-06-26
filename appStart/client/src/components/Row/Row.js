cf5f27536ffef29bd20f34d7429df074a18043ac
Doug <hathemagicpenguin@gmail.com>
2021-06-26 11:28:14 -0400

    Build Fixes

--- appStart/client/src/components/Row/Row.js
+++ appStart/client/src/components/Row/Row.js
@@ -1,12 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /**
  * @param props
+ * @param props.fluid
+ * @param props.children
  */
-function Row(props) {
-  return (
-    <div className={`row${props.fluid ? "-fluid" : ""}`}>{props.children}</div>
-  );
+function Row({ fluid, children }) {
+  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
+
+Row.propTypes = {
+  fluid: PropTypes.string,
+  children: PropTypes.element
+};
 
 export default Row;
--- appStart/client/src/pages/Recipes.js
+++ appStart/client/src/pages/Recipes.js
@@ -1,7 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /**
- * @param props
+<<<<<<< Updated upstream
+ *
  */
 // eslint-disable-next-line space-before-blocks
 function Recipes(props) {
@@ -17,5 +19,28 @@ function Recipes(props) {
     </div>
   );
 }
+=======
+ * @param props
+ * @param props.recipe
+ */
+// eslint-disable-next-line space-before-blocks
+function Recipes({ recipe }) {
+  return (
+    <div>
+      <h2> The Recipes we are serving today</h2>
+      <ul>
+        {recipe.map((item) => (
+          // eslint-disable-next-line react/jsx-key
+          <li>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Recipes.propTypes = {
+  recipe: PropTypes.array
+};
 
 export default Recipes;
+>>>>>>> Stashed changes
