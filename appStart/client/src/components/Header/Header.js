c5b8c48b6f2104afe75e555df0a7c32301cb20b0
Doug <hathemagicpenguin@gmail.com>
2021-07-06 18:58:58 -0400

    CSS

--- appStart/client/src/components/Header/Header.js
+++ appStart/client/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./Header.css";
 
 // eslint-disable-next-line init-declarations
 
@@ -13,11 +14,15 @@ function Header() {
   // eslint-disable-next-line keyword-spacing
 
   return (
-    <div className="App-header ">
+    <div className="header">
       <h1>
         <Link
           to="/"
-          className={location.pathname === "/" ? "nav-link active" : "nav-link"}
+          className={
+            location.pathname === "/"
+              ? "nav-link active page-title"
+              : "nav-link page-title"
+          }
         >
           Hearth Library
         </Link>
--- appStart/client/src/components/Navbar/Navbar.js
+++ appStart/client/src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./Navbar.css";
 
 // eslint-disable-next-line space-before-blocks
 /**
