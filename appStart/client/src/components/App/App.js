c33cbb1c06c8a40c8bd621c61b93677bc363e19c
Kyle Nance <nancekyle15@gmail.com>
2021-07-07 20:27:59 -0400

    login progress so far, still doesnt authenticate

--- appStart/client/src/components/App/App.js
+++ appStart/client/src/components/App/App.js
@@ -3,29 +3,20 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-//old version
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// eslint-disable-next-line no-unused-vars
 
-// import Home from "./pages/Home";
-// import Categories from "./pages/Categories";
-// import Recipes from "./pages/Recipes";
-// import Login from "./pages/Login";
-// import About from "./pages/About";
-// import Navbar from "./components/Navbar";
-
+//old code
+//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // Only visible when logged in
 import Dashboard from "../Dashboard/Dashboard";
 import Preferences from "../Preferences/Preferences";
-
-import Home from "../../pages/Home";
-import Categories from "../../pages/Categories";
+//import Home from "../../pages/Home";
+//import Categories from "../../pages/Categories";
 import Login from "../../pages/Login/Login";
-import About from "../../pages/About";
+//import About from "../../pages/About";
 // import Navbar from "../Navbar";
 //end of old code
 
-//new version
+//new code
 import { Router, Switch, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { recipeFound } from "../../features/Recipes/recipeSlice";
@@ -33,7 +24,7 @@ import axios from "axios";
 import Home from "../../pages/Home";
 import Categories from "../../pages/Categories";
 import Category from "../../pages/Category";
-import Login from "../../pages/Login";
+//import Login from "../../pages/Login";
 import About from "../../pages/About";
 import Results from "../../pages/Results";
 //end of new code
@@ -43,7 +34,8 @@ import Recipe from "../../pages/Recipe";
 import Navbar from "../Navbar/Navbar";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-import useToken from "./useToken";
+// import useToken from "./useToken";
+import { useToken } from "../../pages/Login/Login";
 
 import history from "../History/history";
 
@@ -53,17 +45,12 @@ import history from "../History/history";
 
 
 function App() {
-  const { token, setToken } = useToken();
+  // const { token, setToken } = useToken();
 
-  if (!token) {
-    return <Login setToken={setToken} />
-  }
+  // if (!token) {
+  //   return <Login setToken={setToken} />
+  // }
 
-  return (
-    <div>
-      <div>
-        <Header />
-      </div>
   const dispatch = useDispatch();
 
   /**
@@ -97,7 +84,7 @@ function App() {
         <Route path="/categories/:category" component={Category} />;
         <Route exact path="/recipes" component={Recipes} />
         <Route path="/recipes/:title" component={Recipe} />;
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/login" > <Login setToken={useToken}> </Login></Route >
         <Route exact path="/about" component={About} />
         <Route exact path="/search" component={Results} />
       </Switch>
--- appStart/client/src/components/App/useToken.js
+++ appStart/client/src/components/App/useToken.js
@@ -1,21 +1,21 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function useToken() {
-    const getToken = () => {
-        const tokenString = localStorage.getItem("token");
-        const userToken = JSON.parse(tokenString);
-        return userToken?.token
-    };
+// export default function useToken() {
+//     const getToken = () => {
+//         const tokenString = localStorage.getItem("token");
+//         const userToken = JSON.parse(tokenString);
+//         return userToken?.token
+//     };
 
-    const [token, setToken] = useState(getToken());
+//     const [token, setToken] = useState(getToken());
 
-    const saveToken = userToken => {
-        localStorage.setItem("token", JSON.stringify(userToken));
-        setToken(userToken.token);
-    };
+//     const saveToken = userToken => {
+//         localStorage.setItem("token", JSON.stringify(userToken));
+//         setToken(userToken.token);
+//     };
 
-    return {
-        setToken: saveToken,
-        token
-    }
-}
+//     return {
+//         setToken: saveToken,
+//         token
+//     }
+// }
